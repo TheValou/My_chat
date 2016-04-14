ca8f67a22ad4df4ce1a9b1838e47fdc1cad91abc
Curtis110 <wurfl.valentin@gmail.com>
2016-04-14 17:45:47 +0200

    modification avec ejs

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+//var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options", { layout: false } );
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/login', function(req, res) {
+  res.render('auth');
 });
 
+app.get('/', function(req, res){
+  res.render('index');            
+
+});
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+  console.log('a user connected');
 
   socket.on('disconnect', function(){
     console.log('deconnecté');
   })
 
   socket.on('petit_nouveau', function(pseudo) {
-      socket.pseudo = pseudo;
-    })
+    socket.pseudo = pseudo;
+  })
 
   socket.on('chat message', function (message) {
     console.log(message);
     var date = new Date();
-    message += date.toDateString();
-    message += socket.pseudo;
+    var time = date.getHours() + ":" + date.getMinutes();
+    date = date.toDateString() +' à '+time;
+    var pseudo = socket.pseudo;
     //socket.broadcast.emit('chat message', message); //tout le monde sauf sender
-    io.emit('chat message', message);
+    io.emit('chat message', message, pseudo, date, time);
+
   })
 
 });
