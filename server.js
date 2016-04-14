d1cf5ff4666900b1123361db36c89839f237bcc6
Curtis110 <wurfl.valentin@gmail.com>
2016-04-14 11:32:10 +0200

    première version du chat

---
+++ server.js
@@ -0,0 +1,74 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('deconnecté');
+  })
+
+  socket.on('petit_nouveau', function(pseudo) {
+      socket.pseudo = pseudo;
+    })
+
+  socket.on('chat message', function (message) {
+    console.log(message);
+    var date = new Date();
+    message += date.toDateString();
+    message += socket.pseudo;
+    //socket.broadcast.emit('chat message', message); //tout le monde sauf sender
+    io.emit('chat message', message);
+  })
+
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:3000');
+});
+
+// var http = require('http');
+// var fs = require('fs');
+// // Chargement de socket.io
+// var io = require('socket.io').listen(server);
+
+// // Chargement du fichier index.html affiché au client
+// var server = http.createServer(function(req, res) {
+// 	fs.readFile('./index.html', 'utf-8', function(error, content) {
+// 		res.writeHead(200, {"Content-Type": "text/html"});
+// 		res.end(content);
+// 	});
+// });
+
+
+
+// // Quand un client se connecte, on le note dans la console
+// io.sockets.on('connection', function (socket) {
+
+// 	socket.emit('message', 'Vous êtes bien connecté !');
+
+// 	socket.broadcast.emit('message', 'Attzntion, nouvelle recrue sur le Chat !!');
+
+     // Quand le serveur reçoit un signal de type "message" du client
+     // socket.on('chat message', function (message) {
+     //    console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
+     //        socket.broadcast.emit('chat message', message);
+     //    });
+
+//     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
+//     socket.on('petit_nouveau', function(pseudo) {
+//     	socket.pseudo = pseudo;
+//     	socket.broadcast.emit('message', 'Attention, nouvelle recrue sur le Chat : '+ socket.pseudo + ' !!');
+
+//     });
+
+//  });
+
+// server.listen(8080);
