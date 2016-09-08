b8a6a08e0bde4c0665fcdb0b6e91e8c30d3b3163
TheValou <wurfl.valentin@gmail.com>
2016-09-08 15:04:38 +0200

    ajouter les sessions avec Passport

--- server.js
+++ server.js
@@ -44,44 +44,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(8000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:8000');
 });
-
-// var http = require('http');
-// var fs = require('fs');
-// // Chargement de socket.io
-// var io = require('socket.io').listen(server);
-
-// // Chargement du fichier index.html affiché au client
-// var server = http.createServer(function(req, res) {
-// 	fs.readFile('./index.html', 'utf-8', function(error, content) {
-// 		res.writeHead(200, {"Content-Type": "text/html"});
-// 		res.end(content);
-// 	});
-// });
-
-
-
-// // Quand un client se connecte, on le note dans la console
-// io.sockets.on('connection', function (socket) {
-
-// 	socket.emit('message', 'Vous êtes bien connecté !');
-
-// 	socket.broadcast.emit('message', 'Attzntion, nouvelle recrue sur le Chat !!');
-
-     // Quand le serveur reçoit un signal de type "message" du client
-     // socket.on('chat message', function (message) {
-     //    console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
-     //        socket.broadcast.emit('chat message', message);
-     //    });
-
-//     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-//     socket.on('petit_nouveau', function(pseudo) {
-//     	socket.pseudo = pseudo;
-//     	socket.broadcast.emit('message', 'Attention, nouvelle recrue sur le Chat : '+ socket.pseudo + ' !!');
-
-//     });
-
-//  });
-
-// server.listen(8080);
